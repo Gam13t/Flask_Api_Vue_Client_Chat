d35d6a8c225b38969e0ae48e1bb11c221716153e
Gam13t <53529326+Gam13t@users.noreply.github.com>
2020-12-01 23:38:14 +0200

    Added some more parts of client

---
+++ Client/src/components/Login.vue
@@ -0,0 +1,98 @@
+<template>
+    <div class="login">
+      <div v-if="proccessing" class="text-center"> Please wait... </div>
+      <div v-if="message" class="text-center"> {{message}} </div>
+
+      <b-form-input
+        v-model="username"
+        type="text"
+        class="input-form"
+        placeholder="Username">
+      </b-form-input>
+
+      <b-form-input
+        v-model="password"
+        class="input-form"
+        type="password"
+        placeholder="Password">
+      </b-form-input>
+
+      <b-button
+        v-on:click="login"
+        variant="primary"
+        class="btn-block"
+      >
+      Log me in
+     </b-button>
+      <b-button
+        v-on:click="register"
+        variant="light"
+        class="btn-block"
+      >
+      Register
+      </b-button>
+
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      username: "",
+      password: "",
+      proccessing: false,
+      message: ""
+    };
+  },
+  methods: {
+    login: function () {
+      this.loading = true;
+      this.axios
+          .post("/api/login", {
+            username: this.username,
+            password: this.password
+          })
+          .then(response => {
+            if (response.data.status == "success") {
+              this.proccessing = false;
+              this.$emit("authenticated", true, response.data.data);
+            } else {
+              this.message = "Login Faild, try again";
+            }
+          })
+    },
+    register: function () {
+      this.loading = true;
+      this.axios
+          .post("/api/register", {
+            username: this.username,
+            password: this.password
+          })
+          .then(response => {
+            if (response.data.status == "success") {
+              this.message = "User created successfully, now click the login button"
+              this.proccessing = false
+            } else {
+              this.message = "Registration Failed, try again";
+            }
+          })
+    }
+  }
+};
+</script>
+
+<style scoped>
+.login {
+  width: 500px;
+  border: 1px solid #cccccc;
+  background-color: #ffffff;
+  margin: auto;
+  margin-top: 200px;
+  padding: 20px;
+}
+.input-form {
+  margin-bottom: 9px;
+}
+</style>
---
+++ Client/src/components/Messages.vue
@@ -0,0 +1,34 @@
+<template>
+   <div>
+    <div v-for="(message, id) in messages" v-bind:key="id">
+        <div class="chat-message col-md-5"
+          v-bind:class="[(message.from_user == active_chat) ? 'to-message' : 'from-message offset-md-7']">
+          {{message.message}}
+        </div>
+    </div>
+   </div>
+</template>
+<script>
+export default {
+  props: {
+    messages: Array,
+    active_chat: Number
+  }
+};
+</script>
+<style>
+.from-message {
+  background: #30AA15;
+  color: white;
+  border-radius: 3px;
+  padding: 8px 2px;
+  margin-bottom: 4px;
+}
+.to-message {
+  background: rgb(201, 209, 209);
+  color: rgb(57, 103, 100);
+  border-radius: 3px;
+  padding: 8px 2px;
+  margin-bottom: 4px;
+}
+</style>
